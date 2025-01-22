88340a1e91b0e25561eb17fa4c5c5c1f478a4694
steph sandoval <stephsandovalc@gmail.com>
2025-01-22 14:59:22 -0600

    primeng installation - toast example

--- IMDb-Lite/src/app/app.component.ts
+++ IMDb-Lite/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toast, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [MessageService]
 })
 export class AppComponent {
   title = 'IMDb-Lite';
+  constructor(private messageService: MessageService) {}
+
+  show() {
+    this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Message Content', life: 3000 });
+  }
 }
