7452e9a715da9dbca71d463cd490b86b5056cf3f
PavelCA04 <bzamora@estudiantec.cr>
2025-01-23 14:25:36 -0600

    Login Component Start

--- IMDb-Lite/src/app/app.routes.ts
+++ IMDb-Lite/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: 'auth',
+    component: LoginPageComponent
+}
+
+];
--- IMDb-Lite/src/app/auth/pages/login-page/login-page.component.ts
+++ IMDb-Lite/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [CardModule, ButtonModule, InputTextModule, FloatLabelModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onLogin(): void {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      console.log('Login successful:', { username, password });
+    } else {
+      console.error('Form is invalid');
+    }
+  }
 }
+
