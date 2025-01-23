a39df75e9b7399af877826c449450751fcc093a0
PavelCA04 <bzamora@estudiantec.cr>
2025-01-22 22:59:11 -0600

    Component Initialization

---
+++ IMDb-Lite/src/app/IMDb/pages/movie-page/movie-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-page',
+  imports: [],
+  templateUrl: './movie-page.component.html',
+  styleUrl: './movie-page.component.scss'
+})
+export class MoviePageComponent {
+
+}
