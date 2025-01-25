f46dab8de9234cbafa355b461cd37a5fce3fdc0e
PavelCA04 <bzamora@estudiantec.cr>
2025-01-24 23:50:51 -0600

    Carousel/Header Advance

--- IMDb-Lite/src/app/IMDb/shared/main-header/main-header.component.ts
+++ IMDb-Lite/src/app/IMDb/shared/main-header/main-header.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-main-header',
-  imports: [],
+  imports: [CarouselModule, ButtonModule],
   templateUrl: './main-header.component.html',
   styleUrl: './main-header.component.scss'
 })
 export class MainHeaderComponent {
-
+  carouselItems = [
+    {
+      image: 'background.png',
+      title: 'Inception',
+      description: 'A brief description about the movie Inception.'
+    },
+    {
+      image: 'darkknight.png',
+      title: 'The Dark Knight',
+      description: 'A brief description about The Dark Knight.'
+    },
+    {
+      image: 'interstellar.png',
+      title: 'Interstellar',
+      description: 'A brief description about Interstellar.'
+    }
+  ];
 }
+
--- IMDb-Lite/src/app/app.routes.ts
+++ IMDb-Lite/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { MainHeaderComponent } from './IMDb/shared/main-header/main-header.component';
 
 export const routes: Routes = [
 {
     path: 'auth',
     component: LoginPageComponent
+},
+{
+    path: 'imdb',
+    component: MainHeaderComponent
 }
-
 ];
