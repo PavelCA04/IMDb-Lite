fb22ca66dede5bf6cb5a48587a4cc23c1e85f7a5
PavelCA04 <bzamora@estudiantec.cr>
2025-01-27 10:31:36 -0600

    Registration Dialog

---
+++ IMDb-Lite/src/app/auth/pages/registration-dialog/registration-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-dialog',
+  imports: [],
+  templateUrl: './registration-dialog.component.html',
+  styleUrl: './registration-dialog.component.scss'
+})
+export class RegistrationDialogComponent {
+
+}
