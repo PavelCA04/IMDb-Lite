a2991f487efb8574b3625cc85e363f371c5e0f07
PavelCA04 <bzamora@estudiantec.cr>
2025-02-01 00:18:23 -0600

    Registration Dialog Setup for connection with DB

--- IMDb-Lite/src/app/auth/pages/registration-dialog/registration-dialog.component.ts
+++ IMDb-Lite/src/app/auth/pages/registration-dialog/registration-dialog.component.ts
@@ -5,12 +5,17 @@ import { InputTextModule } from 'primeng/inputtext';
 import { SelectModule } from 'primeng/select';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { FormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { CommonModule } from '@angular/common';
+import { IMDbService } from '../../../IMDb/services/imdb.service';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-registration-dialog',
-  imports: [Dialog, ButtonModule, InputTextModule, SelectModule, FormsModule, FloatLabelModule],
+  imports: [Dialog, ButtonModule, InputTextModule, SelectModule, FormsModule, FloatLabelModule, CommonModule, ToastModule],
   templateUrl: './registration-dialog.component.html',
-  styleUrl: './registration-dialog.component.scss'
+  styleUrl: './registration-dialog.component.scss',
+  providers: [MessageService]
 })
 export class RegistrationDialogComponent {
   @Input() visible: boolean = false;
@@ -19,6 +24,8 @@ export class RegistrationDialogComponent {
   name: string = '';
   email: string = '';
   password: string = '';
+
+  constructor(private registrationService: IMDbService, private messageService: MessageService) {}
 
   get isFormInvalid(): boolean {
     return !this.name.trim() || !this.email.trim() || !this.password.trim();
@@ -40,16 +47,31 @@ export class RegistrationDialogComponent {
     this.visibleChange.emit(this.visible);
   }
   
+
   register() {
-    if (this.isFormInvalid) return;
+    console.log('register', this.name, this.email, this.password);
+    /*
+    if (this.isFormInvalid) {
+      this.messageService.add({ severity: 'warn', summary: 'Warning', detail: 'All fields are required.' });
+      return;
+    }
 
-    console.log("User Registered:", { name: this.name, email: this.email, password: this.password });
+    this.registrationService.register(this.name, this.email, this.password).subscribe({
+      next: () => {
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Registration successful!' });
+        this.resetForm();
+        this.closeDialog();
+      },
+      error: () => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Registration failed. Try again.' });
+      }
+    });*/
+  }
 
+  private resetForm() {
     this.name = '';
     this.email = '';
     this.password = '';
-
-    this.closeDialog();
   }
 }
 
