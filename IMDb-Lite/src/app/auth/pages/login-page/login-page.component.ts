84114f11d324c50b45579f44bb9b5bd0ece5a952
PavelCA04 <bzamora@estudiantec.cr>
2025-02-01 00:18:03 -0600

    Login Setup for connection with DB

--- IMDb-Lite/src/app/auth/pages/login-page/login-page.component.ts
+++ IMDb-Lite/src/app/auth/pages/login-page/login-page.component.ts
@@ -6,30 +6,48 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { RegistrationDialogComponent } from '../registration-dialog/registration-dialog.component';
 import { DialogModule } from 'primeng/dialog';
+import { MessageService } from 'primeng/api';
+import { CommonModule } from '@angular/common';
+import { IMDbService } from '../../../IMDb/services/imdb.service';
+import { ToastModule } from 'primeng/toast';
 @Component({
   selector: 'app-login-page',
-  imports: [CardModule, ButtonModule, InputTextModule, FloatLabelModule, ReactiveFormsModule, RegistrationDialogComponent, DialogModule],
+  imports: [CardModule, ButtonModule, InputTextModule, FloatLabelModule, ReactiveFormsModule, RegistrationDialogComponent, DialogModule, CommonModule, ToastModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
+  providers: [MessageService]
 })
 export class LoginPageComponent {
   loginForm: FormGroup;
   isRegistrationDialogVisible: boolean = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private messageService: MessageService, private loginService: IMDbService) {
     this.loginForm = this.fb.group({
       username: ['', [Validators.required]],
       password: ['', [Validators.required]],
     });
   }
 
-  onLogin(): void {
-    if (this.loginForm.valid) {
-      const { username, password } = this.loginForm.value;
-      console.log('Login successful:', { username, password });
-    } else {
-      console.error('Form is invalid');
+  onLogin() {
+    console.log('onLogin', this.loginForm.value);
+    /*
+    if (this.loginForm.invalid) {
+      this.messageService.add({ severity: 'warn', summary: 'Warning', detail: 'Please fill in all fields.' });
+      return;
     }
+
+    const { username, password } = this.loginForm.value;
+
+    this.loginService.login(username, password).subscribe({
+      next: (response: any) => {
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Login successful!' });
+        console.log('User logged in:', response);
+      },
+      error: (err: any) => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Invalid username or password.' });
+        console.error('Login failed:', err);
+      }
+    });*/
   }
 
   showRegistrationDialog() {
