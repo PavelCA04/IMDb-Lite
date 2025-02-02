1890ccfcfd03447920a329076a11bd19cd50d0da
Fabricio Solis Alpizar <fabriciosolisalpizar@gmail.com>
2025-02-01 23:33:03 -0600

    fix: Problems with ObjectId at the moment of read movies

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -18,7 +18,7 @@ const allowedOrigins = [
 const corsOptions: cors.CorsOptions = {
   origin: allowedOrigins,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
 };
 
