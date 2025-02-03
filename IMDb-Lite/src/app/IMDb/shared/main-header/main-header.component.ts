77ebe6843263fd644c7df4acdec627561ea06a6a
steph sandoval <stephsandovalc@gmail.com>
2025-02-03 14:02:42 -0600

    header

--- IMDb-Lite/src/app/IMDb/shared/main-header/main-header.component.ts
+++ IMDb-Lite/src/app/IMDb/shared/main-header/main-header.component.ts
@@ -19,19 +19,24 @@ export class MainHeaderComponent {
 
   carouselItems = [
     {
-      image: 'background.png',
-      title: 'Inception',
-      description: 'A brief description about the movie Inception.'
+      image: 'https://www.wallpaperflare.com/static/362/256/53/harry-potter-and-the-deathly-hallows-harry-potter-daniel-radcliffe-harry-wallpaper.jpg',
+      title: 'Harry Potter',
     },
     {
-      image: 'darkknight.png',
-      title: 'The Dark Knight',
-      description: 'A brief description about The Dark Knight.'
+      image: 'https://images.alphacoders.com/765/thumb-1920-765810.jpg',
+      title: 'Fantastic Beasts',
     },
     {
-      image: 'interstellar.png',
-      title: 'Interstellar',
-      description: 'A brief description about Interstellar.'
+      image: 'https://wallpaperaccess.com/full/198680.jpg',
+      title: 'Fast and Furious',
+    },
+    {
+      image: 'https://th.bing.com/th/id/R.4550ae959d3581faf11e7312519638ba?rik=C6%2fyJnDuCPiHxA&riu=http%3a%2f%2fwallpapercave.com%2fwp%2fwp1867297.jpg&ehk=pqQ%2fo3wz5xcsp2OkoGuXl%2bA4RLDDE5bmN3S%2bDeKhxAc%3d&risl=&pid=ImgRaw&r=0',
+      title: 'John Wick',
+    },
+    {
+      image: 'https://www.wallpaperflare.com/static/823/606/524/transformers-age-of-extinction-movies-transformers-age-wallpaper.jpg',
+      title: 'Transformers',
     }
   ];
 
